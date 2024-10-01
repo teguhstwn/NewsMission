6160e1132937bfea24a8f1510dc38c3d2a2acf7f
Teguh Setiawan <teguhs.sgr@gmail.com>
2024-10-01 17:04:10 +0700

    match lib version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.teguh.setiawan.newsmission"
         minSdk = 26
-        targetSdk = 34
+        targetSdk = 33
         versionCode = 1
         versionName = "1.0"
 
--- bookmark/build.gradle.kts
+++ bookmark/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 }
 android {
     namespace = "com.teguh.newsmission.bookmark"
-    compileSdk = 34
+    compileSdk = 33
 
     defaultConfig {
         minSdk = 26
