24115dbfec1cbd73cb5c7ba26b11998511b3be76
Teguh Setiawan <teguhs.sgr@gmail.com>
2024-10-01 17:51:02 +0700

    test again

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
     defaultConfig {
         applicationId = "com.teguh.setiawan.newsmission"
         minSdk = 26
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
